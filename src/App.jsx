b1dac75277484e86377345091b5c1a9d41641a16
Mehul Mohan <mehulmpt@gmail.com>
2021-01-13 01:53:37 +0530

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Video from './Video'
 import './App.css'
 
+const API_URL =
+	'https://raw.githubusercontent.com/codedamn-classrooms/tiktok-react-material/main/data.json'
+
 export default function App() {
+	const [videos, setVideos] = useState([])
+
+	useEffect(() => {
+		// TODO: Get the data from API_URL above
+		// Store it inside videos state variable
+		fetch(API_URL)
+			.then((data) => data.json())
+			.then((data) => setVideos(data))
+	}, [])
+
 	return (
 		<div className="app">
 			<div className="container">
-				<Video url="https://github.com/codedamn-classrooms/tiktok-react-material/raw/main/v1.mp4" />
+				{videos.map((video) => {
+					return <Video url={video.url} />
+				})}
 			</div>
 		</div>
 	)
