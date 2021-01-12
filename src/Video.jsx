b5588aeb2c7f75d6f9db2e1b77abed38bf7bdcdd
Mehul Mohan <mehulmpt@gmail.com>
2021-01-12 23:52:45 +0530

    lab

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,47 +1,12 @@
-import React, { useEffect, useRef } from 'react'
-import Footer from './FooterLeft'
-import Sidebar from './FooterRight'
+import React from 'react'
 import './Video.css'
 
 export default function Video(props) {
-	const {
-		id,
-		url,
-		channel,
-		description,
-		song,
-		likes,
-		messages,
-		shares,
-		playing,
-		setPlaying
-	} = props
-
-	const videoRef = useRef(null)
-
-	useEffect(() => {
-		if (playing !== id) {
-			videoRef.current.pause()
-		}
-	}, [playing])
-
-	const onVideoPress = () => {
-		if (videoRef.current.paused) {
-			videoRef.current.play()
-			setPlaying(id)
-		} else {
-			videoRef.current.pause()
-			setPlaying(null)
-		}
-	}
+	const { url } = props
 
 	return (
 		<div className="video">
-			<video className="player" ref={videoRef} loop onClick={onVideoPress} src={url}></video>
-			<div className="bottom-controls">
-				<Footer channel={channel} description={description} song={song} />
-				<Sidebar likes={likes} messages={messages} shares={shares} />
-			</div>
+			<video className="player" loop src={url}></video>
 		</div>
 	)
 }
