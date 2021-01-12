11b184d24614a37a48b0c4cc2792d59d51f9e347
Mehul Mohan <mehulmpt@gmail.com>
2021-01-13 02:55:40 +0530

    lab

--- src/Video.jsx
+++ src/Video.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import './Video.css'
 
 export default function Video(props) {
-	const { url } = props
+	const { url, channel, description, song } = props
 
 	return (
 		<div className="video">
 			<video className="player" loop src={url}></video>
+			<div className="bottom-controls">
+				<Footer channel={channel} description={description} song={song} />
+			</div>
 		</div>
 	)
 }
