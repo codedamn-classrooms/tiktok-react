b6586d66708b139405b7f71339e0635c9dee26c6
Mehul Mohan <mehulmpt@gmail.com>
2021-08-19 07:09:47 +0530

    Update FooterLeft.jsx

--- src/FooterLeft.jsx
+++ src/FooterLeft.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import MusicNote from './icons/MusicNote'
 import './FooterLeft.css'
 
 export default function VideoFooter(props) {
+	const { channel, description, song } = props
 	// write code here
-	return null
+	return <div className="footer-left">
+	<div className="text">
+		<h3>@{channel}</h3>
+		<p>{description}</p>
+		<div className="ticker">
+			<MusicNote style={{ width: '30px' }} />
+			<marquee direction="left" scrollamount="2">
+				{song}
+			</marquee>
+		</div>
+	</div>
+</div>
 }
